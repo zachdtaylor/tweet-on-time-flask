4dffd47f043c9d413e22b709d0f3c0ee9377d5c8
Zach Taylor <zachtylr04@gmail.com>
2020-08-27 23:24:15 -0500

    Build basic UI to add tweets to scheduler

--- tweetontime/tweet.py
+++ tweetontime/tweet.py
@@ -5,10 +5,17 @@ from werkzeug.exceptions import abort
 
 from .auth import login_required
 from .db import get_db
+from .tweet_on_time import get_profile_info, schedule_tweet
 
 bp = Blueprint('tweet', __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('tweet/index.html')
+    if request.method == 'POST':
+        body = request.form['body']
+        tweet_date = request.form['tweet-date']
+        tweet_time = request.form['tweet-time']
+        schedule_tweet(body, f'{tweet_date} {tweet_time}')
+    profile = get_profile_info()
+    return render_template('tweet/index.html', profile=profile)
