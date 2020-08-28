eaf6d1c5bbb521dcd5574ae75540ae998b28dfc8
Zach Taylor <zachtylr04@gmail.com>
2020-08-27 23:25:04 -0500

    Use OAuth1 to authenticate to twitter api

--- tweetontime/auth.py
+++ tweetontime/auth.py
@@ -6,6 +6,7 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from .db import get_db
+from .tweet_on_time import tweet_scheduler
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -39,6 +40,7 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -58,11 +60,13 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
+            # tweet_scheduler.start()
             return redirect(url_for('index'))
 
         flash(error)
 
     return render_template('auth/login.html')
+
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -75,10 +79,12 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
 
 def login_required(view):
     @functools.wraps(view)
--- tweetontime/settings.py
+++ tweetontime/settings.py
@@ -4,3 +4,7 @@ import os
 load_dotenv()
 
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+API_KEY = os.getenv("API_KEY")
+API_SECRET_KEY = os.getenv("API_SECRET_KEY")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
