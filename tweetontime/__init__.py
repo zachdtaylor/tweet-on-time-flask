a865a56ae983580481793521787d3e2c7f2e8c1f
Zach Taylor <zachtylr04@gmail.com>
2020-09-10 23:37:15 -0500

    Allow secret key to be passed via env variable

--- tweetontime/__init__.py
+++ tweetontime/__init__.py
@@ -3,17 +3,17 @@ import os
 from datetime import datetime
 from flask import Flask
 from .tweet_on_time import tweet_scheduler
+from .settings import SECRET_KEY
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'tweetontime.sqlite')
-    )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_mapping(
+            SECRET_KEY=SECRET_KEY or 'dev',
+            DATABASE=os.path.join(app.instance_path, 'tweetontime.sqlite')
+        )
     else:
         app.config.from_mapping(test_config)
 
