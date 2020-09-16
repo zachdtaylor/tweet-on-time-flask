83433070a17fb1b90904310332b97b15693f741f
Zach Taylor <zachtylr04@gmail.com>
2020-09-15 23:18:51 -0500

    Allow db connection to be used in multiple threads

--- tweetontime/models/db.py
+++ tweetontime/models/db.py
@@ -12,7 +12,8 @@ def get_db():
     if not conn:
         conn = sqlite3.connect(
             settings.DATABASE['PATH'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            detect_types=sqlite3.PARSE_DECLTYPES,
+            check_same_thread=False
         )
         conn.row_factory = sqlite3.Row
 
